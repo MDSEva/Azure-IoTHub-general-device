b60f7bcaadbd638ffe5975702b092633e12830e1
Hancom MDS Inc <seteam@outlook.kr>
2021-05-14 10:50:29 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@
 #!/bin/bash
 # set the STRING variable
 export IOTHUB_DEVICE_SECURITY_TYPE="DPS"
-export IOTHUB_DEVICE_DPS_ENDPOINT="global.azure-devices-provisioning.net"
-export IOTHUB_DEVICE_DPS_ID_SCOPE="0ne000FFA42"
-export IOTHUB_DEVICE_DPS_DEVICE_ID="6cf3e2b0-baf5-4ed9-ad19-fe42e1f91a88"
-export IOTHUB_DEVICE_DPS_DEVICE_KEY="6C2kSTZX8n7C4s0d61bUH7LjwmCszkiOiv0E1xOnYkg="
-export IOTHUB_DEVICE_CONNECTION_STRING="HostName=min-certi-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=pjNhmZu92+xj9G8fe1ZT4ZMtL36GOcRzdJ0zUdYWjwo="
+export IOTHUB_DEVICE_DPS_ENDPOINT=""
+export IOTHUB_DEVICE_DPS_ID_SCOPE=""
+export IOTHUB_DEVICE_DPS_DEVICE_ID=""
+export IOTHUB_DEVICE_DPS_DEVICE_KEY=""
+export IOTHUB_DEVICE_CONNECTION_STRING=""
 #export KEYPAD_INTERRUPT="DISABLE"
 export KEYPAD_INTERRUPT="ENABLE"
 
